576009b5da7cf8ef39df3149cdc2299711f5664f
vtw <vtiaraw2@gmail.com>
2024-11-04 14:54:19 +0700

    commit

---
+++ app/src/main/java/com/dicoding/myrecyclerview/MainActivity.kt
@@ -0,0 +1,158 @@
+package com.dicoding.myrecyclerview
+
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var rvHeroes: RecyclerView
+    private val list = ArrayList<Hero>()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        rvHeroes = findViewById(R.id.rv_heroes)
+        rvHeroes.setHasFixedSize(true)
+
+        list.addAll(getListHeroes())
+        showRecyclerList()
+    }
+
+    private fun enableEdgeToEdge() {}
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.action_list -> {
+                rvHeroes.layoutManager = LinearLayoutManager(this)
+            }
+            R.id.action_grid -> {
+                rvHeroes.layoutManager = GridLayoutManager(this, 2)
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun getListHeroes(): ArrayList<Hero> {
+        val dataName = resources.getStringArray(R.array.data_name)
+        val dataDescription = resources.getStringArray(R.array.data_description)
+        val dataPhoto = resources.getStringArray(R.array.data_photo)
+        val minLength = minOf(dataName.size, dataDescription.size, dataPhoto.size)
+        val listHero = ArrayList<Hero>()
+        for (i in 0 until minLength) {
+            val hero = Hero(dataName[i], dataDescription[i], dataPhoto[i])
+            listHero.add(hero)
+        }
+        return listHero
+    }
+
+    private fun showRecyclerList() {
+        rvHeroes.layoutManager = LinearLayoutManager(this)
+        val listHeroAdapter = ListHeroAdapter(list)
+        rvHeroes.adapter = listHeroAdapter
+
+        listHeroAdapter.setOnItemClickCallback(object : ListHeroAdapter.OnItemClickCallback {
+            override fun onItemClicked(data: Hero) {
+                showSelectedHero(data)
+            }
+        })
+    }
+
+    private fun showSelectedHero(hero: Hero) {
+        Toast.makeText(this, "Kamu memilih " + hero.name, Toast.LENGTH_SHORT).show()
+    }
+}
+//package com.dicoding.myrecyclerview
+//
+//import android.os.Bundle
+//import android.view.Menu
+//import android.widget.Toast
+//import androidx.activity.enableEdgeToEdge
+//import androidx.appcompat.app.AppCompatActivity
+//import androidx.core.view.ViewCompat
+//import androidx.core.view.WindowInsetsCompat
+//import androidx.recyclerview.widget.LinearLayoutManager
+//import androidx.recyclerview.widget.RecyclerView
+//
+//class MainActivity : AppCompatActivity() {
+//    private lateinit var rvHeroes: RecyclerView
+//    private val list = ArrayList<Hero>()
+//    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        enableEdgeToEdge()
+//        setContentView(R.layout.activity_main)
+//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+//            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+//            insets
+//        }
+//        rvHeroes = findViewById(R.id.rv_heroes)
+//        rvHeroes.setHasFixedSize(true)
+//
+//        list.addAll(getListHeroes())
+//        showRecyclerList()
+//    }
+//
+//    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+//        menuInflater.inflate(R.menu.menu_main, menu)
+//        return super.onCreateOptionsMenu(menu)
+//    }
+//
+//    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+//        when (item.itemId) {
+//            R.id.action_list -> {
+//                rvHeroes.layoutManager = LinearLayoutManager(this)
+//            }
+//            R.id.action_grid -> {
+//                rvHeroes.layoutManager = GridLayoutManager(this, 2)
+//            }
+//        }
+//        return super.onOptionsItemSelected(item)
+//    }
+//
+//    private fun getListHeroes(): ArrayList<Hero> {
+//        val dataName = resources.getStringArray(R.array.data_name)
+//        val dataDescription = resources.getStringArray(R.array.data_description)
+//        val dataPhoto = resources.obtainTypedArray(R.array.data_photo)
+//        val listHero = ArrayList<Hero>()
+//        for (i in dataName.indices) {
+//            val hero = Hero(dataName[i], dataDescription[i], dataPhoto.getResourceId(i, -1))
+//            listHero.add(hero)
+//        }
+//        return listHero
+//    }
+//
+//    private fun showRecyclerList() {
+//        rvHeroes.layoutManager = LinearLayoutManager(this)
+//        val listHeroAdapter = ListHeroAdapter(list)
+//        rvHeroes.adapter = listHeroAdapter
+//        listHeroAdapter.setOnItemClickCallback(object : ListHeroAdapter.OnItemClickCallback {
+//            override fun onItemClicked(data: Hero) {
+//                showSelectedHero(data)
+//            }
+//        })
+//    }
+//
+//    private fun showSelectedHero(hero: Hero) {
+//        Toast.makeText(this, "Kamu memilih " + hero.name, Toast.LENGTH_SHORT).show()
+//    }
+//}
